dd4989f437361d40a4f7b35a64ec29debe404499
burnettbobby <20298092@myclyde.ac.uk>
2022-09-30 14:11:31 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import './css/app.css';
-import login from './components/Login';
-import register from './components/Register';
+import Login from './components/Login';
+import Register from './components/Register';
+
+// const signupModal = document.querySelector(".signup-form-wrapper");
+// const loginModal = document.querySelector(".login-form-wrapper");
+// const signupBtn = document.querySelector(".signup-btn");
+// const loginBtn = document.querySelector(".login-btn");
+// const signupX = document.querySelector(".signup-x");
+// const loginX = document.querySelector(".login-x");
+// const formContainer = document.querySelector(".form-container");
+
+// signupBtn.addEventListener("click", () => {
+//   signupModal.classList.add("display");
+//   formContainer.classList.add("disable");
+// });
+
+// loginBtn.addEventListener("click", () => {
+//   loginModal.classList.add("display");
+//   formContainer.classList.add("disable");
+// });
+
+// signupX.addEventListener("click", () => {
+//   signupModal.classList.remove("display");
+//   formContainer.classList.remove("disable");
+// });
+
+// loginX.addEventListener("click", () => {
+//   loginModal.classList.remove("display");
+//   formContainer.classList.remove("disable");
+// });
+
 
 function App() {
   return (
@@ -16,8 +45,10 @@ function App() {
       </div>
       <div className="form-container">
         {/* signup container */}
-        
+        <Login/>
+
         {/* login container */}
+        <Register/>
       </div>
     </div>
 
